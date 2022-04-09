5d5a5cf1bcf522bd233ab97ce799665a0ee4aed5
bbobrek <bartosz.bobrek1993@gmail.com>
2022-04-09 16:21:40 +0200

    java config exercises

---
+++ jee-spring-java/src/main/java/pl/coderslab/beans/Captain.java
@@ -0,0 +1,9 @@
+package pl.coderslab.beans;
+
+public class Captain {
+
+    public void startSailing() {
+        System.out.println("Start sailing...");
+    }
+
+}
---
+++ jee-spring-java/src/main/java/pl/coderslab/beans/Ship.java
@@ -0,0 +1,15 @@
+package pl.coderslab.beans;
+
+public class Ship {
+
+    private Captain captain;
+
+    public Ship(Captain captain) {
+        this.captain = captain;
+    }
+
+    public Captain getCaptain() {
+        return captain;
+    }
+
+}

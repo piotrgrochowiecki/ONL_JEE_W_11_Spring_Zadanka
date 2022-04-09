a49a28d0a136f26f487f131e4bd5a1069ac45dd7
bbobrek <bartosz.bobrek1993@gmail.com>
2022-04-09 14:16:44 +0200

    xml config exercises

--- jee-spring-xml/src/main/java/pl/coderslab/app/SpringDiApplication.java
+++ jee-spring-xml/src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -1,21 +1,10 @@
 package pl.coderslab.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.coderslab.beans.EmailService;
 import pl.coderslab.beans.HelloWorld;
-
-/**
- *
- * Utwórz klasę HelloWorld w pakiecie pl.coderslab.beans z atrybutem typu String oraz nazwie message.
- * Utwórz konstruktor ustawiający pole message, zdefiniuj gettery i settery.
- * Utwórz ziarno w konfiguracji xml o nazwie helloWorld klasy pl.coderslab.beans.HelloWorld.
- * Uzupełnij konfigurację ziaren, by przy pomocy konstruktora wstrzyknąć do klasy HelloWorld wartość Witaj Spring dla atrybutu message.
- * Uzupełnij klasę HelloWorld o poniższą metodę:
- * public void printMessage() {
- *       System.out.println("Your Message : " + message);
- * }
- *
- * Pobierz ziarno w metodzie main a następnie wywołaj na nim metodę printMessage.
- * */
+import pl.coderslab.beans.MessageSender;
+import pl.coderslab.beans.MessageService;
 
 public class SpringDiApplication {
 
@@ -26,6 +15,15 @@ public class SpringDiApplication {
         //HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
         HelloWorld helloWorld = context.getBean("helloWorld", HelloWorld.class);
         helloWorld.printMessage();
+
+        System.out.println(helloWorld.getMessage());
+
+        MessageService messageService = (EmailService) context.getBean("emailService");
+        messageService.send();
+
+        MessageSender messageSender = context.getBean("messageSender", MessageSender.class);
+        messageSender.sendMessage();
+        messageSender.sendMessageFromProperty();
 
         context.close();
     }
---
+++ jee-spring-xml/src/main/java/pl/coderslab/beans/MessageService.java
@@ -0,0 +1,9 @@
+package pl.coderslab.beans;
+
+public interface MessageService {
+
+    void send();
+
+    void send(String message);
+
+}
